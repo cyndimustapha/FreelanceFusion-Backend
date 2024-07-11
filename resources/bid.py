79a95e2a2bfbb8c24d7cbb7912bd6122977402d0
cyndi mustapha <marrencyndi101@gmail.com>
2024-07-11 14:52:05 +0300

    updating bid resource

--- resources/bid.py
+++ resources/bid.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource, reqparse
-from sqlalchemy import and_, not_
-from flask_jwt_extended import jwt_required, get_jwt
-from models import db, Bid  
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from models import db, Bid, Job
 
 class BidResource(Resource):
     
@@ -12,17 +11,36 @@ class BidResource(Resource):
         parser.add_argument('job_id', type=int, required=True, help='Job ID must be provided')
         args = parser.parse_args()
 
-        current_user = get_jwt().identity
-        if not current_user:
+        current_user_id = get_jwt_identity()
+        if not current_user_id:
             return {'message': 'User not authenticated'}, 401
+
+        job = Job.query.get(args['job_id'])
+        if not job:
+            return {'message': 'Job not found'}, 404
 
         bid = Bid(
             amount=args['amount'],
             job_id=args['job_id'],
-            freelancer_id=current_user.id
+            freelancer_id=current_user_id
         )
 
         db.session.add(bid)
         db.session.commit()
 
         return {'message': 'Bid placed successfully', 'bid_id': bid.id}, 201
+
+    @jwt_required
+    def get(self, job_id):
+        current_user_id = get_jwt_identity()
+        if not current_user_id:
+            return {'message': 'User not authenticated'}, 401
+
+        job = Job.query.get(job_id)
+        if not job:
+            return {'message': 'Job not found'}, 404
+
+        bids = Bid.query.filter_by(job_id=job_id).all()
+        serialized_bids = [{'id': bid.id, 'amount': bid.amount, 'freelancer_id': bid.freelancer_id} for bid in bids]
+
+        return {'bids': serialized_bids}, 200
