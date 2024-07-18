50bfdabacf90e61e5c0a1c92f1ac0872d1840d93
kenkipkoec <itsmetrokenaki@gmal.com>
2024-07-18 23:44:48 +0300

    debugged

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///FreelanceFusion.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'super-secret'
+    JWT_ACCESS_TOKEN_EXPIRES = 86400
