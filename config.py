efdd630b76512f25bebe3202f67d3dc979cf0f41
cyndi mustapha <marrencyndi101@gmail.com>
2024-07-23 20:58:12 +0300

    hosting updates

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///FreelanceFusion.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'super-secret'
     JWT_ACCESS_TOKEN_EXPIRES = 86400
